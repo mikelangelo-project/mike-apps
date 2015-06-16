4fd2310efac9a784cde7c97b3d3f0a98f9e2fce1
Gregor Berginc <gregor.berginc@xlab.si>
2015-06-16 12:13:52 +0200

    Example of running OpenFOAM with console output.

--- OpenFOAM/module.py
+++ OpenFOAM/module.py
@@ -1,3 +1,4 @@
 from osv.modules import api
 
+#default = api.run("--env=WM_PROJECT_DIR=/openfoam --redirect=/log /usr/bin/simpleFoam -case /openfoam/case")
 default = api.run("--env=WM_PROJECT_DIR=/openfoam /usr/bin/simpleFoam -case /openfoam/case")
