8b298a705c536c7ab1e6d80d47ac00f18016dcb4
Gregor Berginc <gregor.berginc@xlab.si>
2016-03-15 12:07:51 +0100

    Update sysbench app
    The patch uses the new format of user manifest that requires a space after a colon separating target and local file.
    
    It also changes the default test to run memory experiment with 4 threads.
    
    Change-Id: I4b1a6b0d20fa6a746df98fa36e5c779da7ee38cd
    Signed-off-by: Gregor Berginc <gregor.berginc@xlab.si>

--- sysbench/module.py
+++ sysbench/module.py
@@ -1,3 +1,4 @@
 from osv.modules import api
 
-default = api.run("/usr/bin/sysbench --test=cpu --cpu-max-prime=100000 run")
+#default = api.run("/usr/bin/sysbench --test=cpu --cpu-max-prime=100000 --num-threads=4 run")
+default = api.run("/usr/bin/sysbench --test=memory --num-threads=4 run")
