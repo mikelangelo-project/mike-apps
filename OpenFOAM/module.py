4eee860c753340e308bcefc9084f19cbeb79f7a9
Gregor Berginc <gregor.berginc@xlab.si>
2017-03-01 09:37:40 +0100

    Do not set the default run command
    OpenFOAM app is usually bundled along with the CLI module that sets it's
    own default command line (`/cli/cli.so`). Previously, OpenFOAM app would
    override this causing the display of the  help message of the
    `simpleFoam` solver and immediate termination of the VM.
    
    This patch removes the default command. Some examples are still given in
    the `module.py` file, but need to be manually enabled by the user of
    this app.
    
    Change-Id: Id073bf13f70c5f13c621629890ee4fb80077cbca
    Signed-off-by: Gregor Berginc <gregor.berginc@xlab.si>

--- OpenFOAM/module.py
+++ OpenFOAM/module.py
@@ -3,4 +3,4 @@ from osv.modules import api
 api.require('open-mpi')
 #default = api.run("--env=WM_PROJECT_DIR=/openfoam --redirect=/log /usr/bin/simpleFoam -case /openfoam/case")
 #default = api.run("--env=WM_PROJECT_DIR=/openfoam /usr/bin/simpleFoam.so -case /openfoam/case")
-default = api.run("--env=WM_PROJECT_DIR=/openfoam /usr/bin/simpleFoam.so -help")
+#default = api.run("--env=WM_PROJECT_DIR=/openfoam /usr/bin/simpleFoam.so -help")
